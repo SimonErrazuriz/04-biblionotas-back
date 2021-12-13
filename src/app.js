68be264ecc16e3f3349d9802d74dae3ad202a686
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2021-12-13 19:57:22 -0300

    Revert "fix: cors"
    This reverts commit 2155c86f5a713da445c7d4aa791be2f6c55caeb2.

--- src/app.js
+++ src/app.js
@@ -15,12 +15,6 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
-app.use(function(req, res, next) {
-    // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "https://biblionotas.netlify.app"); 
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 app.use(session({
     secret: 'secret',
     resave: true,
