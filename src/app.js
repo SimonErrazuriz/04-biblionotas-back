e0c40118cd94813f67a3d6faa61359bb69af66f7
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2021-12-14 15:52:18 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -13,18 +13,6 @@ const port = process.env.PORT || 3000
 app.set("port", port);
 
 /* Middlewares */
-var allowlist = ['http://localhost:4200/', 'https://biblionotas.netlify.app/']
-var corsOptionsDelegate = function (req, callback) {
-    var corsOptions;
-    if (allowlist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-    } else {
-        corsOptions = { origin: false } // disable CORS for this request
-    }
-    callback(null, corsOptions) // callback expects two parameters: error and options
-}
-
-app.use(cors(corsOptionsDelegate));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(session({
@@ -36,8 +24,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 /* Cargar las rutas */
-app.use("/api/fichas", require('./routes/fichas'));
-app.use("/api/usuarios", require('./routes/usuarios'));
+app.use("/api/fichas", cors(), require('./routes/fichas'));
+app.use("/api/usuarios", cors(), require('./routes/usuarios'));
 
 /* Exportar */
 module.exports = app;
