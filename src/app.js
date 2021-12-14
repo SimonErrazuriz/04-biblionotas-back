374af419a838af951cba7962b57555e1c094fd22
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2021-12-14 14:54:08 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 require('dotenv').config();
 const cors = require('cors');
-const corsOptions = {
-    origin: true,
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(allowCrossDomain);
 const express = require('express');
 const session = require('express-session');
