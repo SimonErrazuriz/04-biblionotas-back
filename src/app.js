028534233c985d6d1fb5839c6d9f5679d3515504
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2021-12-13 21:54:47 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
+const cors = require('cors');
+const corsOptions ={
+    origin: true, 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use(allowCrossDomain);
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
@@ -13,20 +21,6 @@ app.set("port", process.env.PORT || 3000);
 /* Middlewares */
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-app.use(allowCrossDomain);
 app.use(session({
     secret: 'secret',
     resave: true,
