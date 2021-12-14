2b7cf73ec0215747e3cbb72848519221ab283e23
Simón Errázuriz <simonerrazurizrubilar@gmail.com>
2021-12-14 15:17:33 -0300

    fix

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 mongoose
-    .connect(process.env.MONGODB_URI || 'mongodb://localhost/04-fichas-academicas')
+    .connect('mongodb+srv://simon:r4e3w2q1@cluster0.avd46.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://localhost/04-fichas-academicas')
     .then(db => console.log('Conectado a la base de datos'))
     .catch(err => console.error(err));
